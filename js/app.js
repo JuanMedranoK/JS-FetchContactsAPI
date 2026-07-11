f3a29737ca3ec4b6594397b359f69778e1188e5a
Juan Carlos <juan_carlos_medrano@hotmail.com>
2026-07-11 17:12:45 -0400

    Implement Fetch API integration

--- js/app.js
+++ js/app.js
@@ -3,3 +3,14 @@ const listaContactos = document.getElementById("resultado");
 
 boton.addEventListener("click", cargarContactos);
 
+function cargarContactos() {
+
+    fetch('https://raydelto.org/agenda.php')
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(response){
+            console.log(response);
+        });
+        
+}
